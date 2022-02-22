042054d6313b997e4bac27765d13baaeb21b34d4
CristinaPicazo <derezade@gmail.com>
2022-02-22 14:30:01 +0100

    gulp added

---
+++ front/js/dist/script.js
@@ -0,0 +1,20 @@
+const url = 'http://127.0.0.1:3000/api/products';
+fetch(url)
+    .then(response => {
+        const json = response.json()
+        return json;
+    })
+    .then(data => {
+        displayItems(data)
+    })
+    .catch(err => console.log(err))
+
+function displayItems(sofas) {
+    for (let i = 0; i < sofas.length; i++) {
+        console.log(sofas[i]);
+    }
+    const section = document.getElementById('items')
+    section.innerHTML = section
+    console.log(section)
+}
+
