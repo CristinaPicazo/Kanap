27e5e95758b2adf284bea0ef2729569487ee3fda
CristinaPicazo <derezade@gmail.com>
2022-03-06 23:55:27 +0100

    delete tried

--- front/js/cart.js
+++ front/js/cart.js
@@ -19,60 +19,92 @@ for (let i = 0; i < localStorage.length; i++) {
 let totalQuantity = document.getElementById('totalQuantity');
 let sumTotalQuantity = "";
 let totalPrice = document.getElementById('totalPrice');
-let deleteItems = document.getElementsByClassName('deleteItem');
 
 
 
 //Show items in cart
 for (let i = 0; i < cartItemsArray.length; i++) {
     let cartItems = document.getElementById('cart__items');
+
     cartItems.innerHTML += `
-                        <article class="cart__item" data-id="${cartItemsArray[i]._id}" data-color="${cartItemsArray[i].selectedColor}">
-                                        <div class="cart__item__img">
-                                        <img src=${cartItemsArray[i].imageUrl} alt=${cartItemsArray[i].altTxt}>
-                                        </div>
-                                        <div class="cart__item__content">
-                                        <div class="cart__item__content__description">
-                                            <h2>${cartItemsArray[i].name}</h2>
-                                            <p>${cartItemsArray[i].selectedColor}</p>
-                                            <p>${cartItemsArray[i].price}</p>
-                                        </div>
-                                        <div class="cart__item__content__settings">
-                                            <div class="cart__item__content__settings__quantity">
-                                            <p>Qté : </p>
-                                            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value=${cartItemsArray[i].selectedQuantity}>
-                                            </div>
-                                            <div class="cart__item__content__settings__delete">
-                                            <p class="deleteItem">Delete</p>
-                                            </div>
-                                        </div>
-                                        </div>
-                                    </article>
-                        `;
+                                <article class="cart__item" data-id="${cartItemsArray[i]._id}" data-color="${cartItemsArray[i].selectedColor}">
+                                                <div class="cart__item__img">
+                                                <img src=${cartItemsArray[i].imageUrl} alt=${cartItemsArray[i].altTxt}>
+                                                </div>
+                                                <div class="cart__item__content">
+                                                <div class="cart__item__content__description">
+                                                    <h2>${cartItemsArray[i].name}</h2>
+                                                    <p>${cartItemsArray[i].selectedColor}</p>
+                                                    <p>${cartItemsArray[i].price}</p>
+                                                </div>
+                                                <div class="cart__item__content__settings">
+                                                    <div class="cart__item__content__settings__quantity">
+                                                    <p>Qté : </p>
+                                                    <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value=${cartItemsArray[i].selectedQuantity}>
+                                                    </div>
+                                                    <div class="cart__item__content__settings__delete">
+                                                    <p class="deleteItem">Delete</p>
+                                                    </div>
+                                                </div>
+                                                </div>
+                                            </article>
+                                `;
 
 
     //TO DO
-    //Delete Sofa    
+    //Delete Sofa 
+
+
+    /*
+    productRows.forEach(function(productRow){
+        var product = productRow.querySelector(".product");
+        var price = productRow.querySelector(".price");
+        var quantity = productRow.querySelector(".quantity");
+        var order = product[i].innerText +" | "+ price[i].innerText +" | "+ quantity[i].innerText;
+        console.log(order);
+    });
+    deleteItems[i].addEventListener('click', function () {
+        //deleteItems[i] = undefined,
+        alert('delete click')
+    }, { once: true });
+
     cartItemsArray.forEach(element => {
         delete cartItemsArray[element];
     });
-    /*
+
+    const filterSofas = cartItemsArray.filter(sofas => 
+        sofas != "");
+
+
         deleteItems[i].addEventListener('click', () => {
             delete cartItemsArray[i];
             if (cartItemsArray[i + 1] != undefined) {
                 cartItemsArray[i] = cartItemsArray[i - 1]
             }
         });
-    */
+    
 
     //TO DO
     //Totals
     //let sumTotalQuantity = parseInt(cartItemsArray[i].selectedQuantity);
-    sumTotalQuantity += cartItemsArray[i].selectedQuantity;
+    const sumTotalQuantity += cartItemsArray[i].selectedQuantity;
     totalQuantity.innerHTML = sumTotalQuantity;
     totalPrice.innerHTML += parseFloat(cartItemsArray[i].selectedQuantity * cartItemsArray[i].price);
     //.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
+    */
+    let deleteItems = document.getElementsByClassName('deleteItem');
+
+
 }
+
+function deleteSofa() {
+    deleteItems.addEventListener('click', function () {
+        //deleteItems[i] = undefined,
+        alert('delete click')
+    }, { once: true });
+}
+
+
 
 //Order Sofa
 const contact = {
